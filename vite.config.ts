9a124b0df67897be0344fee06e57233489aa5732
sakura-social <ywairong6@gmail.com>
2026-05-26 12:27:43 +0800

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     svgLoader(),
   ],
-  base: '/sakura-social/',
+  base: '/element-plus-vue3-dashboard/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
